c160978da2fa5a91f3955eb349f01ea9a0e754c6
KingOwls <147095998+KingOwls@users>
2024-02-26 08:46:59 -0500

    2602202402

--- JS/main.js
+++ JS/main.js
@@ -1,110 +1,122 @@
 const HeroesMarvel = [
     {
         "id": "001",
+        "Universo": "marvel",
         "about": "Iron Man es el alter-ego de Tony Stark, un filántropo multimillonario que, luego de un secuestro, debe construir un aparato para mantenerse con vida. Esto lo lleva a crear un traje de alta tecnología que eventualmente usa como herramienta para luchar contra el crimen.",
         "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
+        "squarePic": "./../IMG/squarePic_IronMan.jpg",
         "name": "Iron Man"
     },
     {
         "id": "002",
         "about": "",
+        "Universo": "marvel",
         "picture": "./../IMG/Picture_SpiderMan.jpg",
-        "squarePic": "",
+        "squarePic": "./../IMG/squarePic_SpiderMan.jpg",
         "name": "Spider-Man"
     },
     {
         "id": "003",
         "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
+        "Universo": "marvel",
+        "picture": "./../IMG/Picture_CaptainAmerica.jpg",
+        "squarePic": "./../IMG/squarePic_CaptainAmerica.jpg",
         "name": "Capitán América"
     },
     {
         "id": "004",
         "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
+        "Universo": "marvel",
+        "picture": "./../IMG/Picture_DoctorStrange.jpg",
+        "squarePic": "./../IMG/squarePic_DoctorStrange.jpg",
         "name": "Doctor Strange"
     },
     {
         "id": "005",
         "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
+        "Universo": "marvel",
+        "picture": "./../IMG/Picture_ScarletWitch.jpg",
+        "squarePic": "./../IMG/squarePic_ScarletWitch.jpg",
         "name": "Scarlet Witch"
     },
     {
         "id": "006",
         "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
-        "name": "Black Widow"
+        "Universo": "marvel",
+        "picture": "./../IMG/Picture_Starlord.jpg",
+        "squarePic": "./../IMG/squarePic_StarLord.jpg",
+        "name": "Star Lord"
     },
     {
         "id": "007",
         "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
+        "Universo": "marvel",
+        "picture": "./../IMG/Picture_BlackPanther.jpg",
+        "squarePic": "./../IMG/squarePic_Blackpanther.jpg",
         "name": "Black Panther"
     },
     {
         "id": "008",
         "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
+        "Universo": "marvel",
+        "picture": "./../IMG/Picture_TheHulk.jpg",
         "squarePic": "",
         "name": "The Hulk"
     },
-    
-]
-const HeroesDC = [
     {
-        "id": "001",
+        "id": "009",
         "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
+        "Universo": "DC",
+        "picture": "./../IMG/Picture_SuperMan.jpg",
+        "squarePic": "./../IMG/squarePic_SuperMan.jpg",
         "name": "SuperMan"
-    },
-    {
-        "id": "002",
-        "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
-        "name": "BatMan"
-    },
-    {
-        "id": "003",
-        "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
-        "name": "Wonder Woman"
-    },
-    {
-        "id": "004",
-        "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
-        "name": "Linterna Verde"
-    },
-    {
-        "id": "006",
-        "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
-        "name": "Shazam"
     },
     {
         "id": "010",
         "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
-        "name": "John Constantine"
+        "Universo": "DC",
+        "picture": "./../IMG/Picture_Batman.jpg",
+        "squarePic": "./../IMG/squarePic_Batman.jpg",
+        "name": "BatMan"
     },
     {
         "id": "011",
         "about": "",
-        "picture": "./../IMG/Picture_IronMan.jpg",
-        "squarePic": "",
-        "name": "Hawkgirl"
+        "Universo": "DC",
+        "picture": "./../IMG/Picture_WonderWoman.jpg",
+        "squarePic": "./../IMG/squarePic_WonderWoman.jpg",
+        "name": "Wonder Woman"
+    },
+    {
+        "id": "012",
+        "about": "",
+        "Universo": "DC",
+        "picture": "./../IMG/Picture_DetectiveMarciano.jpg",
+        "squarePic": "./../IMG/squarePic_DetectiveMarciano.jpg",
+        "name": "Detective Marciano"
+    },
+    {
+        "id": "013",
+        "about": "",
+        "Universo": "DC",
+        "picture": "./../IMG/Picture_Shazam.jpg",
+        "squarePic": "./../IMG/squarePic_Shazam.jpg",
+        "name": "Shazam"
+    },
+    {
+        "id": "014",
+        "about": "",
+        "Universo": "DC",
+        "picture": "./../IMG/Picture_JohnConstaine.jpg",
+        "squarePic": "./../IMG/squarePic_JohnConstaine.jpg",
+        "name": "John Constantine"
+    },
+    {
+        "id": "015",
+        "about": "",
+        "Universo": "DC",
+        "picture": "./../IMG/Picture_JonahHex.jpg",
+        "squarePic": "./../IMG/squarePic_JonahHex.jpg",
+        "name": "Jonah Hex"
     },
 ]
